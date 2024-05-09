e2a389e31954818566822468392c6b1c6558ee0f
hp <mbatrajpr635@gmail.com>
2024-05-09 14:21:39 +0530

    Added genericrubixcube Model

---
+++ genericrubixcube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hp on 09-05-2024.
+//
+
+#include "genericrubixcube.h"
---
+++ genericrubixcube.h
@@ -0,0 +1,14 @@
+//
+// Created by hp on 09-05-2024.
+//
+
+#ifndef RUBIXCUBE_GENERICRUBIXCUBE_H
+#define RUBIXCUBE_GENERICRUBIXCUBE_H
+
+
+class genericrubixcube {
+
+};
+
+
+#endif //RUBIXCUBE_GENERICRUBIXCUBE_H
